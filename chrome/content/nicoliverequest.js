070c7886621b1e3129d5d030e836ed2c9ac7afa2
amano <amano@miku39.jp>
2010-01-08 20:05:03 +0900

    ??????????????????

--- chrome/content/nicoliverequest.js
+++ chrome/content/nicoliverequest.js
@@ -555,10 +555,12 @@ var NicoLiveRequest = {
 			this.playlist_start = 0;
 		    }
 		    let nextmusic = NicoLiveHelper.stock[ this.playlist_start ];
-		    this.playlist_start++;
-		    debugprint(nextmusic.video_id+"を再生します");
-		    this.opentab.contentDocument.wrappedJSObject.location.href = "http://www.nicovideo.jp/watch/"+nextmusic.video_id;
-		    this.playlist_first = true;
+		    if(nextmusic){
+			this.playlist_start++;
+			debugprint(nextmusic.video_id+"を再生します");
+			this.opentab.contentDocument.wrappedJSObject.location.href = "http://www.nicovideo.jp/watch/"+nextmusic.video_id;
+			this.playlist_first = true;
+		    }
 		    break;
 		}
 	    }else{
