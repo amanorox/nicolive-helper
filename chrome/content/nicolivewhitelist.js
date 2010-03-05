2541640ea4a40af63ab55ed7ebb237b20ae439a2
amano <amano@miku39.jp>
2010-03-05 21:21:30 +0900

    P????space_ship??????????????ship????????? NanahoshiP?????????????????????.

--- chrome/content/nicolivewhitelist.js
+++ chrome/content/nicolivewhitelist.js
@@ -21,7 +21,7 @@ THE SOFTWARE.
  */
 
 var pname_whitelist = [
-    "supercell","rlboro",
+    "supercell","rlboro","NanahoshiP",
     "aquascape", "BIRUGE", "DixieFlatline", "fourier-drive",
     "instinctive", "kame", "Lue", "MAX_VEGETABLE", "nabe_nabe",
     "neve", "ShakeSphere", "Sweet-Revenge", "saihane", "samfree",
