f2ccc538661dadc97ea387b83fa3755751c4fad2
amano <amano@miku39.jp>
2010-03-06 13:14:50 +0900

    popup notification?????.

--- chrome/content/libs.js
+++ chrome/content/libs.js
@@ -225,6 +225,19 @@ function ShowNotice(txt){
 			    }, 15*1000 );
 }
 
+function ShowPopupNotification(title,text){
+    try {
+	Components.classes['@mozilla.org/alerts-service;1'].getService(Components.interfaces.nsIAlertsService)
+	    .showAlertNotification(null, title, text, false, '', null);
+    } catch(e) {
+	// prevents runtime error on platforms that don't implement nsIAlertsService
+	var image = null;
+	var win = Components.classes['@mozilla.org/embedcomp/window-watcher;1'].getService(Components.interfaces.nsIWindowWatcher)
+	    .openWindow(null, 'chrome://global/content/alerts/alert.xul','_blank', 'chrome,titlebar=no,popup=yes', null);
+	win.arguments = [image, title, text, false, ''];
+    }
+}
+
 function SetWindowTopMost(w,b){
     var Ci = Components.interfaces;
     var XULWindow = w
