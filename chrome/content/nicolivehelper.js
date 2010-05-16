fbc79b1dc22cc79ce39c416c3ff4fcbba661c94e
amano <amano@miku39.jp>
2010-05-17 01:07:17 +0900

    P????APPEND????.

--- chrome/content/nicolivehelper.js
+++ chrome/content/nicolivehelper.js
@@ -655,7 +655,7 @@ var NicoLiveHelper = {
 	    return true;
 	}
 	if(str.match(/(PSP|アイドルマスターSP|m[a@]shup|drop|step|overlap|vocaloid_map|mikunopop|mikupop|ship|dump)$/i)) return false;
-	if(str.match(/(M[A@]D|joysound|MMD|HD|3D|vocaloud|world|頭文字D|イニシャルD|(吸血鬼|バンパイア)ハンターD|4D|TOD|oid|clannad|2nd|3rd|second|third)$/i)) return false;
+	if(str.match(/(M[A@]D|joysound|MMD|HD|3D|vocaloud|world|頭文字D|イニシャルD|(吸血鬼|バンパイア)ハンターD|4D|TOD|oid|clannad|2nd|3rd|second|third|append)$/i)) return false;
 	let t = str.match(/.*([^jO][pP]|jP)[)]?$/);
 	if(t){
 	    return true;
