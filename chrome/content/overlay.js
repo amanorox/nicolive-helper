10df57c4c973300cb0d50afc5719e8e6d9545b3e
amano <amano@miku39.jp>
2010-10-08 22:29:36 +0900

    1.1.18-release

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -776,7 +776,7 @@ var NicoLiveOverlay = {
 		Prefs.setCharPref("version",current);
 		// バージョンが異なるとき、すなわちアップグレードしたときに実行するコードを挿入します。
 		window.setTimeout(function(){
-				      gBrowser.selectedTab = gBrowser.addTab("http://code.google.com/p/nicolivehelper/wiki/UpdateHistory#1.1.17");
+				      gBrowser.selectedTab = gBrowser.addTab("http://code.google.com/p/nicolivehelper/wiki/UpdateHistory#1.1.18");
 				  }, 1500);
 	    }
 	}
