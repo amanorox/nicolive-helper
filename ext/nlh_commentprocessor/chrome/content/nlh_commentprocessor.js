e2cab2724205b091e79ab21e9e4f4fb214fc4433
amano <amano@miku39.jp>
2010-10-31 11:44:36 +0900

    ??????????????????????.

--- ext/nlh_commentprocessor/chrome/content/nlh_commentprocessor.js
+++ ext/nlh_commentprocessor/chrome/content/nlh_commentprocessor.js
@@ -23,36 +23,106 @@ THE SOFTWARE.
 var NicoLiveCommentProcessor = {
 
     addAutoKotehan:function(userid,name){
+	if( !$('nlhaddon-allowoverwrite').checked ){ // 上書きしない.
+	    if( NicoLiveComment.namemap[userid] ){ // すでに設定済み.
+		return;
+	    }
+	}
 	NicoLiveComment.addKotehanDatabase(userid,name);
 	NicoLiveComment.updateCommentViewer();
 	NicoLiveComment.createNameList();
     },
 
-    newProcessComment:function(xmlchat){
-	NicoLiveCommentProcessor.oldprocesscomment(xmlchat);
+    // http://www.nicovideo.jp/user/... から登録(サムネから取れない時)
+    addRefProfileRegister2:function(user_id){
+	if( NicoLiveComment.namemap[user_id] ){ // すでに設定済み.
+	    return;
+	}
+
+	let req = new XMLHttpRequest();
+	if( !req ) return;
+	req.onreadystatechange = function(){
+	    if( req.readyState==4 && req.status==200 ){
+		try{
+		    let text = req.responseText;
+		    let name = text.match(/<h2><strong>(.*)<\/strong>/)[1];
+		    if( name ){
+			NicoLiveComment.addKotehanDatabase(user_id,name);
+			NicoLiveComment.updateCommentViewer();
+			NicoLiveComment.createNameList();
+		    }
+		} catch (x) {
+		}
+	    }
+	};
+	req.open('GET', 'http://www.nicovideo.jp/user/'+user_id );
+	req.send("");
+    },
+
+    // http:/ext.nicovideo.jp/thumb_user/... から登録(こちら優先)
+    addRefProfileRegister:function(user_id){
+	if( NicoLiveComment.namemap[user_id] ){ // すでに設定済み.
+	    return;
+	}
+
+	let req = new XMLHttpRequest();
+	if( !req ) return;
+	req.onreadystatechange = function(){
+	    if( req.readyState==4 && req.status==200 ){
+		try{
+		    let text = req.responseText;
+		    let name = text.match(/><strong>(.*)<\/strong>/)[1];
+		    if( name ){
+			NicoLiveComment.addKotehanDatabase(user_id,name);
+			NicoLiveComment.updateCommentViewer();
+			NicoLiveComment.createNameList();
+		    }else{
+			NicoLiveCommentProcessor.addRefProfileRegister2(user_id);
+		    }
+		} catch (x) {
+		    NicoLiveCommentProcessor.addRefProfileRegister2(user_id);
+		}
+	    }
+	};
+	req.open('GET', 'http://ext.nicovideo.jp/thumb_user/'+user_id );
+	req.send("");
+    },
+
+    newProcessCommentHook:function(chat){
+	NicoLiveCommentProcessor.oldhook(chat);
 
 	if( !$('auto-kotehan').checked ) return;
-	let chat = NicoLiveHelper.extractComment(xmlchat);
 
 	if(chat.date<NicoLiveHelper.connecttime){ return; } // 過去ログ無視.
 	if(chat.premium>=2) return;
-	let dat = chat.text.match(/[@＠]([^0-9０-９\s][^\s@]*?)$/);
+
+	let dat = chat.text.match(/[@＠]([^0-9０-９\s@＠][^\s@＠]*?)$/);
 	if(dat){
 	    let name = dat[1];
-	    if( name=="初見" ) return;
-	    this.addAutoKotehan(chat.user_id,name);
+	    if( name=="初見" || name=="確認" || name=="アンケート" || name=="削除" || name=="代理" ) return;
+	    if( name=="○" || name=="×" || name=="△" || name=="□" || name=="◎") return;
+	    NicoLiveCommentProcessor.addAutoKotehan(chat.user_id,name);
 	}
-	if(chat.text.indexOf("ぼか生天気情報")!=-1){
-	    
+
+	if( $('nlhaddon-ref-userprof').checked && chat.user_id.match(/^\d+$/) ){
+	    // ユーザIDからプロフィール参照登録.
+	    NicoLiveCommentProcessor.addRefProfileRegister(chat.user_id);
 	}
+    },
+
+    changeFontScale:function(size){
+	if( !size ){ debugprint('font size is default 9pt.'); size = 9; }
+	$('comment_table').style.fontSize = size+"pt";
+	$('nlhaddon-font-scale-value').value = size + "pt";
     },
 
     init:function(){
 	debugprint('Multi-purpose Comment Processor init.');
-	this.oldprocesscomment = NicoLiveHelper.processComment;
-	NicoLiveHelper.processComment = function(xmlchat){
-	    NicoLiveCommentProcessor.newProcessComment(xmlchat);
+	NicoLiveCommentProcessor.oldhook = NicoLiveHelper.processCommentHook;
+	NicoLiveHelper.processCommentHook = function(chat){
+	    NicoLiveCommentProcessor.newProcessCommentHook(chat);
 	};
+	NicoLiveCommentProcessor.changeFontScale( $('nlhaddon-comment-font-scale').value );
     },
     destroy:function(){
     }
