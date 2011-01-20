f9496c72e31fb2edade19a260ae382f841ee0ec9
amano <amano@miku39.jp>
2011-01-21 08:52:49 +0900

    CRT?????????????? VC2008???????????????????.

--- ext/xpcom_cookiereader/INLHGetCookie.cpp
+++ ext/xpcom_cookiereader/INLHGetCookie.cpp
@@ -181,3 +181,18 @@ static const mozilla::Module kNLHGetCookieModule = {
 
 NSMODULE_DEFN(NLHGetCookieModule) = &kNLHGetCookieModule;
 NS_IMPL_MOZILLA192_NSGETMODULE(&kNLHGetCookieModule)
+
+
+
+#ifdef _MANAGED
+#pragma managed(push, off)
+#endif
+
+BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved )
+{
+    return TRUE;
+}
+
+#ifdef _MANAGED
+#pragma managed(pop)
+#endif
