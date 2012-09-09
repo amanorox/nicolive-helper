ec149723ec73c16df531292b99671ff2b6e9140d
amano <amano@miku39.jp>
2012-09-09 10:53:15 +0900

    2.1.3-release

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -892,7 +892,7 @@ var NicoLiveOverlay = {
 		// バージョンが異なるとき、すなわちアップグレードしたときに実行するコードを挿入します。
 		window.setTimeout(
 		    function(){
-			gBrowser.selectedTab = gBrowser.addTab("http://code.google.com/p/nicolivehelper/wiki/UpdateHistory#2.1.2");
+			gBrowser.selectedTab = gBrowser.addTab("http://code.google.com/p/nicolivehelper/wiki/UpdateHistory#2.1.3");
 		    }, 1500);
 	    }
 	}
